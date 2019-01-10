628574c9aefb9ff56db2bce8e48db80daf5649f6
Siyu Tian <stian@secureworks.com>
2019-01-10 00:06:27 -0500

    Item 19

--- src/Item19_Skeletal_Implementation/EX1/Test.java
+++ src/Item19_Skeletal_Implementation/EX1/Test.java
@@ -3,6 +3,7 @@ package Item19_Skeletal_Implementation.EX1;
 public class Test {
 
     public static void main(String[] args) {
+
         HomeComputer homeComputer = new HomeComputer();
         homeComputer.process();
 
